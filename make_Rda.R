0ae6ac16128b7055f6f494dce6f8124146fcadc3
Brandon Nedwek <bnedwek@gmail.com>
2016-09-01 19:46:04 -0400

    Make Rda files from CSVs

--- make_Rda.R
+++ make_Rda.R
@@ -1,6 +1,7 @@
 library(dplyr)
 
 DATA_DIR <- 'data/FARS2015NationalCSV'
+DEV_DIR  <- '~/Documents/dev/nhtsa-traffic-data'
 
 accidents <- read.csv(paste(DATA_DIR, 'ACC_AUX.CSV', sep='/'), stringsAsFactors=F)
 persons   <- read.csv(paste(DATA_DIR, 'PER_AUX.CSV', sep='/'), stringsAsFactors=F)
@@ -9,3 +10,7 @@ vehicles  <- read.csv(paste(DATA_DIR, 'VEH_AUX.CSV', sep='/'), stringsAsFactors=
 names(accidents) <- tolower(names(accidents))
 names(persons)   <- tolower(names(persons))
 names(vehicles)  <- tolower(names(vehicles))
+
+save(accidents, file=paste(DEV_DIR, 'accidents.Rda', sep='/'))
+save(persons,   file=paste(DEV_DIR, 'persons.Rda', sep='/'))
+save(vehicles,  file=paste(DEV_DIR, 'vehicles.Rda', sep='/'))
